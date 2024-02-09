a52adead33e80ef3d4cc5a39282a8771ec916482
vlukashh <ankalukashenko@gmail.ru>
2024-02-09 18:59:43 +0700

    Рендеринг списков

--- proj/src/main.js
+++ proj/src/main.js
@@ -8,5 +8,18 @@ let app = new Vue({
         inStock: true,
         inventory: 100,
         onSale: "On Sale",
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+
     }
 })
