a49e7645d9a769d6a5ef93b609810a050b0e08bc
vlukashh <ankalukashenko@gmail.ru>
2024-02-09 18:49:05 +0700

    Условный рендеринг

--- proj/src/main.js
+++ proj/src/main.js
@@ -5,6 +5,8 @@ let app = new Vue({
         description: "A pair of warm, fuzzy socks",
         image: "src/assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: "On Sale",
     }
 })
