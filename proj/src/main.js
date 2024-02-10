0ed74950e26813a2fc2acb5d7d6e862107534ac3
vlukashh <ankalukashenko@gmail.ru>
2024-02-10 22:12:30 +0700

    Фомы

--- proj/src/main.js
+++ proj/src/main.js
@@ -11,6 +11,85 @@ Vue.component('product-details', {
         </ul>
     `
 })
+Vue.component('product-review', {
+    template: `
+
+      <form class="review-form" @submit.prevent="onSubmit">
+        <p v-if="errors.length">
+          <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+        </p>
+        <p>
+          <label for="name">Name:</label>
+          <input id="name" v-model="name" placeholder="name">
+        </p>
+
+        <p>
+          <label for="review">Review:</label>
+          <textarea id="review" v-model="review"></textarea>
+        </p>
+
+        <p>
+          <label for="rating">Rating:</label>
+          <select id="rating" v-model.number="rating">
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+          </select>
+        </p>
+
+        <p>Would you recommend this product?</p>
+        <label>
+          Yes
+          <input type="radio" value="Yes" v-model="recommend"/>
+        </label>
+        <label>
+          No
+          <input type="radio" value="No" v-model="recommend"/>
+        </label>
+
+        <p>
+          <input type="submit" value="Submit">
+        </p>
+      </form>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: []
+        }
+    },
+    methods:{
+        onSubmit() {
+            this.errors = []
+            if(this.name && this.review && this.rating && this.recommend) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.recommend = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.recommend) this.errors.push("Recommendation required.")
+            }
+        }
+    }
+})
+
 Vue.component('product', {
     props: {
         premium: {
@@ -46,11 +125,20 @@ Vue.component('product', {
           >
             Add to cart
           </button>
-          <button @click="removeFromCart"
-          >
-            Remove
-          </button>
         </div>
+        <div>
+          <h2>Reviews</h2>
+          <p v-if="!reviews.length">There are no reviews yet.</p>
+          <ul>
+            <li v-for="review in reviews">
+              <p>{{ review.name }}</p>
+              <p>Rating: {{ review.rating }}</p>
+              <p>{{ review.review }}</p>
+            </li>
+          </ul>
+        </div>
+
+        <product-review @review-submitted="addReview"></product-review>
       </div>
     `,
     data() {
@@ -76,6 +164,7 @@ Vue.component('product', {
                     variantQuantity: 0
                 }
             ],
+            reviews: [],
             cart: 0
         }
     },
@@ -90,8 +179,8 @@ Vue.component('product', {
         updateProduct(variantImage) {
             this.image = variantImage
         },
-        removeFromCart: function() {
-            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
     },
     computed: {
@@ -123,14 +212,8 @@ let app = new Vue({
     methods: {
         updateCart(id) {
             this.cart.push(id)
-        },
-        removeItem(id) {
-            for(let i = this.cart.length - 1; i >= 0; i--) {
-                if (this.cart[i] === id) {
-                    this.cart.splice(i, 1);
-                }
-            }
         }
+
     }
 })
 
